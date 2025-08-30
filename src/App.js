a582b2872e3bc54523869c52111e7ac9a9bad2e3
jaikrishnavr01 <jaikrishnavr@outlook.com>
2025-08-30 10:08:50 +0530

    form ui update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
-import Contact from './Pages/Contact';
 import Project from './Pages/Project';
 
 function App() {
@@ -13,7 +12,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/project" element={<Project />} />
-          <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
     </Router>
